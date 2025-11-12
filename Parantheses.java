6a31d366f03def6153abc455b85c8795d7628e6d
Broildler355 <fouadk2024@gmail.com>
2025-11-12 16:19:53 +0000

    1-1)

--- Parantheses.java
+++ Parantheses.java
@@ -1,1 +1,12 @@
-
+//"([]"
+public class Parenthesis {
+    public static boolean isValid(String input) {
+        string y = "";
+        int x = input.length();
+        string z = "";
+        while (x>=1) {
+            y += input.substring(x,x-1);
+            x--;
+        }
+    }
+}
